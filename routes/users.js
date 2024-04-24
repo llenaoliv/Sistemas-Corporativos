f6524f7d53e08ccc1f38e184280f95cbcb24dba1
llenaoliv <110862025+llenaoliv@users.noreply.github.com>
2024-04-23 21:39:09 -0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcrypt');
 
 const db = require('../models');
-const userService= require('../services/userService'); //classe
-const UserService = new userService(db.User); //Construção do objeto
+const userService = require('../services/userService');
+const UserService = new userService(db.User);
 
-const userController = require('../controllers/userController');//classe
-const UserController = new userController(UserService); //construção do onjeto
+const userController = require('../controllers/userController');
+const UserController = new userController(UserService);
 
 /* GET users listing. */
-router.get('/', function(req, res, next) { //nome da rota '/'
+router.get('/', function(req, res, next) {
   res.send('Modulo de usuario está funcionando');
 });
 
-//rora para criar um novo usuario
-router.post('/novoUsuario', function(req,res,next){
-  UserController.create(req,res);
+// Rota para criar um novo usuário
+router.post('/novoUsuario', async function(req, res, next) {
+  const { nome, email, senha } = req.body;
+  try {
+    UserController.create(req, res);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao inserir o novo usuário' });
+  }
 });
 
 router.get('/localizarUsuarioPeloLogin', function(req, res, next) { 
@@ -25,4 +31,5 @@ router.get('/localizarUsuarioPeloLogin', function(req, res, next) {
 router.get('/localizarUsuarioPeloId', function(req, res, next) { 
   UserController.localizarUsuarioPeloId(req,res);
 });
+
 module.exports = router;
