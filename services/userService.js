24bf874d3ab7abeb33579d8cf419a3fe5882007c
lucas teixeira <91092558+DRAVINCK@users.noreply.github.com>
2024-04-23 21:29:58 -0300

    Update userService.js lucas teixeira

--- services/userService.js
+++ services/userService.js
@@ -1,37 +1,41 @@
 //. /services/userService.js
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-class userService{ //criando a service da user
-//construtor da classe recebe a user model
-constructor(userModel){
-    this.User = userModel 
-
+class userService{
+    constructor(userModel){
+        this.User = userModel 
     }
     async cadastrar(nome, email, senha){
-        try{ //metodo service para inserir no banco
-            const novoUser = await this.User.create( //Só avança uando der certo "await"
+        try{
+            const hashedPassword = await bcrypt.hash(senha, saltRounds);
+            const novoUser = await this.User.create(
             {
                     nome:nome,
                     email:email, 
-                    senha:senha
+                    senha:hashedPassword
                  }
             );
-            return novoUser ? novoUser : null; //if ternário, se novoUser for novoUser retorna novoUser caso contrario null
+            novoUser.senha='';
+            return novoUser ? novoUser : null;
         }
-        catch(error){ //se açgo der errado a service fala pra controler que nao deu 
+        catch(error){
             throw error; 
         }
     }
-    //localizar usuario
     async localizarUsuarioPeloLogin(login,senha){
         try{
-            const AllUsers = await this.User.findAll();
-            return AllUsers? AllUsers: null;
+            const user = await this.User.findOne({ where: { login: login } });
+            if (user && await bcrypt.compare(senha, user.senha)) {
+                return user;
+            } else {
+                return null;
+            }
         }
         catch(error){
             throw error;
         }
     }
-
     async localizarUsuarioPeloId(id){
         try{
             const idUser = await this.User.findOne();
